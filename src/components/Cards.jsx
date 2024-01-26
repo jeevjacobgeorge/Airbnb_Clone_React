c476b51f035bf78e4b82220c537e3f7b1f709610
jeevjacobgeorge <jeev@J4Hp>
2024-01-26 21:29:20 +0530

    gave the transititons

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
+import React ,{ useState } from 'react'
 import './Cards.css'
 import star from '../assets/star.png'
 import ellipse from '../assets/ellipse.png'
 const images_path = "./src/assets/"
+
+
 const Cards = () =>{
     return(
         <div className='cards-section'>
             <Card 
-                img_filename="image_1.png"
-                heading="Life lessons with katie Zaferes"
-                price_dollars = {136}
-                rating = {5.0}
-                rating_count = {6}
-                country = "USA"
+                img_filename="img_1.webp"
+                hover_filename="img_1_hover.jpg"
+                heading="Master French Cuisine"
+                price_dollars = {66}
+                rating = {4.0}
+                rating_count = {9}
+                country = "SA"
             />
             <Card
-                img_filename="img_2.jpeg"
-                heading="Learn wedding photography"
+                img_filename="img_grandma.jpg"
+                hover_filename="img_food_hover.webp"
+                heading="Pasta with the Grandmas"
                 price_dollars = {125}
                 rating = {5.0}
                 rating_count = {30}
-                country = "USA"
+                country = "Italy"
             />
             <Card
                 img_filename="img_5.webp"
+                hover_filename="img_egypt.webp"
                 heading="Plan A Trip To Egypt With a Local Insider"
                 price_dollars = {200}
                 rating = {5.0}
@@ -32,31 +37,52 @@ const Cards = () =>{
             />
             <Card
                 img_filename="image_4.jpeg"
+                hover_filename="image_4_hover.avif"
                 heading="Intuitive Tarot Reading and Meditation"
                 price_dollars = {95}
                 rating = {4.6}
                 rating_count = {19}
                 country = "USA"
             />
+            <Card
+                img_filename="img_4.webp"
+                hover_filename="img_4_hover.webp"
+                heading="Tea tasting with a Tea Sommelier"
+                price_dollars = {75}
+                rating = {5.0}
+                rating_count = {8}
+                country = "CA"
+            />
+            <Card
+                img_filename="img_6.jpg"
+                hover_filename="img_5_hover.jpeg"
+                heading="reat yourself the Olympic chef way"
+                price_dollars = {150}
+                rating = {5.0}
+                rating_count = {15}
+                country = "USA"
+            />
         </div>
     )
     }
 const Card = (props) => {
+    const [isHovered, setHover] = useState(false)
   return (
-        <div className='card'>
-            <img src={images_path + props.img_filename} alt="image_1" />
+        <div className='card' onMouseEnter={() => setHover(true) } onMouseLeave={() => setHover(false)}>
+            <div className='card-img'>
+                <img src={isHovered ? images_path + props.hover_filename : images_path + props.img_filename} alt="image_1" />
+            </div>
             <div className='card-details'>
                 <div className='card-details-top'>
-                    <img id ='star' src={star} alt="star"/>
+                    <img id ='star' src={star} alt="star" />
                     <span>{props.rating.toFixed(1)}</span>
                     <span className='gray'>({props.rating_count || 0})</span>
                     <img id='ellipse' src={ellipse} alt="ellipse" />
                     <span className='gray'>{props.country}</span>                    
                 </div>
-                <div className="card-details-bottom">
                     <p id='card-heading'>{props.heading}</p>
                     <p id='card-text'><strong>From ${props.price_dollars}</strong> / person </p>
-                </div>
+
             </div>
         </div>
   )
