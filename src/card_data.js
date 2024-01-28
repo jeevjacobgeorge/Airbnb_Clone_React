f70af3a648f4f75ad26ce65b9e32527c786ce4b5
jeevjacobgeorge <jeev@J4Hp>
2024-01-28 15:20:13 +0530

    Added data.js

---
+++ src/card_data.js
@@ -0,0 +1,66 @@
+const cardData = [
+    {
+      img_filename: "img_1.webp",
+      hover_filename: "img_1_hover.jpg",
+      heading: "Master French Cuisine",
+      price_dollars: 66,
+      rating: 4.0,
+      rating_count: 9,
+      location: "Online",
+      openSpots: 8,
+      openSpots:8
+    },
+    {
+      img_filename: "img_5.webp",
+      hover_filename: "img_egypt.webp",
+      heading: "Plan A Trip To Egypt With a Local Insider",
+      price_dollars: 200,
+      rating: 5.0,
+      rating_count: 2,
+      location: "USA",
+      openSpots:8
+    },
+    {
+      img_filename: "img_grandma.jpg",
+      hover_filename: "img_food_hover.webp",
+      heading: "Pasta with the Grandmas",
+      price_dollars: 125,
+      rating: 5.0,
+      rating_count: 30,
+      location: "Italy",
+      openSpots:3
+    },
+    {
+      img_filename: "image_4.jpeg",
+      hover_filename: "image_4_hover.avif",
+      heading: "Intuitive Tarot Reading and Meditation",
+      price_dollars: 95,
+      rating: 4.6,
+      rating_count: 19,
+      location: "USA",
+      openSpots:4
+    },
+    {
+      img_filename: "img_6.jpg",
+      hover_filename: "img_5_hover.jpeg",
+      heading: "Treat yourself the Olympic chef way",
+      price_dollars: 150,
+      rating: 5.0,
+      rating_count: 15,
+      location: "USA",
+      openSpots:0
+    },
+    {
+      img_filename: "img_tea.jpeg",
+      hover_filename: "img_4_hover.webp",
+      heading: "Tea tasting with a Tea Sommelier",
+      price_dollars: 75,
+      rating: 5.0,
+      rating_count: 8,
+      location: "Online",
+      openSpots:8
+    },
+  ];
+  
+  export default cardData;
+  
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,73 +2,29 @@ import React ,{ useState } from 'react'
 import './Cards.css'
 import star from '../assets/star.png'
 import ellipse from '../assets/ellipse.png'
+import cardData from '../card_data'
 const images_path = "./assets/"
 
 const Cards = () =>{
+    const cards_mapped = cardData.map(item => <Card key={item.img_filename} {...item} />)   
     return(
         <div className='cards-section'>
-            <Card 
-                img_filename="img_1.webp"
-                hover_filename="img_1_hover.jpg"
-                heading="Master French Cuisine"
-                price_dollars = {66}
-                rating = {4.0}
-                rating_count = {9}
-                country = "SA"
-            />
-                <Card
-                    img_filename="img_5.webp"
-                    hover_filename="img_egypt.webp"
-                    heading="Plan A Trip To Egypt With a Local Insider"
-                    price_dollars = {200}
-                    rating = {5.0}
-                    rating_count = {2}
-                    country = "USA"
-                />
-            <Card
-                img_filename="img_grandma.jpg"
-                hover_filename="img_food_hover.webp"
-                heading="Pasta with the Grandmas"
-                price_dollars = {125}
-                rating = {5.0}
-                rating_count = {30}
-                country = "Italy"
-            />
-            <Card
-                img_filename="image_4.jpeg"
-                hover_filename="image_4_hover.avif"
-                heading="Intuitive Tarot Reading and Meditation"
-                price_dollars = {95}
-                rating = {4.6}
-                rating_count = {19}
-                country = "USA"
-            />
-            <Card
-                img_filename="img_6.jpg"
-                hover_filename="img_5_hover.jpeg"
-                heading="Treat yourself the Olympic chef way"
-                price_dollars = {150}
-                rating = {5.0}
-                rating_count = {15}
-                country = "USA"
-            />
-            <Card
-                img_filename="img_tea.jpeg"
-                hover_filename="img_4_hover.webp"
-                heading="Tea tasting with a Tea Sommelier"
-                price_dollars = {75}
-                rating = {5.0}
-                rating_count = {8}
-                country = "CA"
-            />
+           {cards_mapped}
         </div>
     )
     }
 const Card = (props) => {
+    let badgeText = "";
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        console.log("Location prop value:", props.location);
+        badgeText = "ONLINE"
+    }
     const [isHovered, setHover] = useState(false)
-    
   return (
         <div className='card' onMouseEnter={() => setHover(true) } onMouseLeave={() => setHover(false)}>
+            {badgeText && <div className="sold-out-badge">{badgeText}</div> }
             <div className='card-img'>
                 <img src={isHovered ? images_path + props.hover_filename : images_path + props.img_filename} alt="image_1" />
             </div>
@@ -78,7 +34,7 @@ const Card = (props) => {
                     <span>{props.rating.toFixed(1)}</span>
                     <span className='gray'>({props.rating_count || 0})</span>
                     <img id='ellipse' src={ellipse} alt="ellipse" />
-                    <span className='gray'>{props.country}</span>                    
+                    <span className='gray'>{props.location}</span>                    
                 </div>
                     <p id='card-heading'>{props.heading}</p>
                     <p id='card-text'><strong>From ${props.price_dollars}</strong> / person </p>
